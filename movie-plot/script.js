e5bda0a0eb5410524f976a2e937edfac7a6639fb
gabecadiz <gabecadiz@hotmail.com>
2020-02-08 23:20:11 -0500

    setup localStorage
    -setup functions to keep selected seat index, movie and movie ticket price to local storage

--- movie-plot/script.js
+++ movie-plot/script.js
@@ -4,12 +4,46 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// Get data from localStorage and populateUI
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  // populate seats
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  // populate previously selected movie
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+};
+
+populateUI();
+
 // '+' infront of a string, turns it into typeof number
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 // Update total and count
-const updateSelectedCount = () => {
+const updateSelectedCountAndTotal = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
@@ -19,7 +53,8 @@ const updateSelectedCount = () => {
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
-  updateSelectedCount();
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCountAndTotal();
 });
 
 // Seat click event
@@ -30,6 +65,9 @@ container.addEventListener('click', e => {
   ) {
     e.target.classList.toggle('selected');
 
-    updateSelectedCount();
+    updateSelectedCountAndTotal();
   }
 });
+
+// Initial count and total set
+updateSelectedCountAndTotal();
